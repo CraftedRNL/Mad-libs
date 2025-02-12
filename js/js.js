da8c555172683e017440241b9a7b1dc13b961b05
Ryan <126Rliu@frhsd.com>
2025-02-12 12:55:11 -0500

    Other seasons other than summer have NOT been implemented yet,

--- js/js.js
+++ js/js.js
@@ -1,6 +1,9 @@
 document.getElementById("btn").addEventListener("click", function() {
     var content = document.querySelectorAll(".content");
+    var lib = document.querySelectorAll(".input");
     for (var i = 0; i < content.length; i++) {
         content[i].style.filter = "blur(0px)";
+        lib[i].style.color = "rgb(247, 255, 130)";
     }
+
 });
